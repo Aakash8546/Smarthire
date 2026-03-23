0da982566bd598ba45489273602a1d5252263f55
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2026-03-23 20:29:46 +0530

    adding test case

--- src/test/java/com/smarthire/SmarthireApplicationTests.java
+++ src/test/java/com/smarthire/SmarthireApplicationTests.java
@@ -2,12 +2,14 @@ package com.smarthire;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SmarthireApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // This test will now pass
+    }
 }
